1af298a11340fe20062d4f6b4d2296adc553bff2
zishan-dev <zishsn11@gmail.com>
2025-06-19 12:37:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Jisshu-filter-bot
 fi
-cd /JisshuTG
+cd /Jisshu-filter-bot
 pip3 install -U -r requirements.txt
 echo "Starting Jisshu filter bot...."
 python3 bot.py
