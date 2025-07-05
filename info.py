a6c00a97f29da26ba26450741cd416bc09b7c0de
Yohan Rockstar <77877340+yohanrock@users.noreply.github.com>
2025-07-05 18:05:50 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,9 +53,9 @@ FILES_DATABASE = environ.get("FILES_DATABASE", "")
 COLLECTION_NAME = environ.get("COLLECTION_NAME", "INFINITY MOVIES")
 
 # Other Channel's
-SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", ""))
+SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", "-1002810224461"))
 DELETE_CHANNELS = int(environ.get("DELETE_CHANNELS", "0"))
-request_channel = environ.get("REQUEST_CHANNEL", "")
+request_channel = environ.get("REQUEST_CHANNEL", "-1002810224461")
 REQUEST_CHANNEL = (
     int(request_channel)
     if request_channel and id_pattern.search(request_channel)
